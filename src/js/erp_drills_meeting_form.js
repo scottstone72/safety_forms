4f29492a448ebe85a0684cb9678ccf5140f3e2be
Scott Johnstone <scottstone_2@hotmail.com>
2015-02-06 17:14:07 -0700

    v 1.0

---
+++ src/js/erp_drills_meeting_form.js
@@ -0,0 +1,14 @@
+//(function($) {
+//
+//  Drupal.behaviors.erp_drills_meeting = {
+//    attach: function (context, settings) {
+//
+//      $('#add-name', context).click(function () {
+//        $('#attendance-container').css('border', '2px solid red');
+//      });
+//
+//
+//    }
+//  }
+//
+//})(jQuery);
